afcaacf55ceea6cd291bd9e279a488e99471f881
JordanTrz <jordan.terrazos@gmail.com>
2025-04-27 22:06:11 +1000

    add: Gemini and imageKit

--- frontend/src/components/newPrompt/NewPrompt.jsx
+++ frontend/src/components/newPrompt/NewPrompt.jsx
@@ -1,8 +1,20 @@
+import { IKImage } from 'imagekitio-react';
+import Upload from '../upload/Upload';
 import './newPrompt.css';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import model from '../../lib/gemini';
 
 const NewPrompt = () => {
   const endRef = useRef(null);
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: '',
+    dbData: {},
+  });
+
+  const add = async () => {
+    await model('What is the capital of france?');
+  };
 
   useEffect(() => {
     endRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -10,12 +22,21 @@ const NewPrompt = () => {
 
   return (
     <>
+      {img.isLoading && <div>Loading...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData.filePath}
+          width={380}
+          transformation={[{ width: 380 }]}
+        />
+      )}
       <div className="endChat" ref={endRef}></div>
       <form className="newForm" action="">
-        <label htmlFor="file">
-          <img src="/attachment.png" alt="" />
-        </label>
-        <input id="file" type="file" multiple={false} hidden />
+        <Upload setImg={setImg} />
+        <button type="button" onClick={add}>
+          Test
+        </button>
         <input type="text" placeholder="ask anything..." />
         <button>
           <img src="/arrow.png" alt="" />
---
+++ frontend/src/lib/gemini.js
@@ -0,0 +1,29 @@
+import { GoogleGenAI } from '@google/genai';
+
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_PUBLIC_KEY });
+
+const safetySettings = [
+  {
+    category: 'HARM_CATEGORY_HARASSMENT',
+    threshold: 'BLOCK_LOW_AND_ABOVE',
+  },
+  {
+    category: 'HARM_CATEGORY_HATE_SPEECH',
+    threshold: 'BLOCK_LOW_AND_ABOVE',
+  },
+];
+
+async function model(contents) {
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.0-flash',
+    contents,
+    config: {
+      safetySettings,
+    },
+  });
+
+  console.log(response.text);
+  return response.text;
+}
+
+export default model;
